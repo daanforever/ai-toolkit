fix(Adafactor): correct beta2t calculation in optimizer
- Updated the calculation of beta2t to use a constant value based on the decay rate instead of a decreasing function of the step, improving the stability of the optimizer's behavior during training.

toolkit/optimizers/adafactor.py
@@ -519,8 +519,7 @@ class Adafactor(torch.optim.Optimizer):
                 )
                 lr = self._get_lr(group, state)
 
-                beta2t = 1.0 - math.pow(state["step"], group["decay_rate"])
-                beta2t = min(0.99, beta2t)
+                beta2t = 1.0 + group["decay_rate"]
                 eps = group["eps"]
                 if isinstance(eps, tuple) or isinstance(eps, list):
                     eps = eps[0]