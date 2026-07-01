fix(optimizer, Adafactor): ensure non-negative weight decay calculation
- Updated the weight decay calculation in the Adafactor optimizer to ensure it does not fall below zero by using the max function. This change enhances the stability of the optimizer by preventing negative weight decay values during updates.

toolkit/optimizers/adafactor.py
@@ -1256,9 +1256,8 @@ class Adafactor(torch.optim.Optimizer):
                     )
                 )
 
-            group["weight_decay"] = group.get("weight_decay", 0.0) + group.get(
-                "weight_decay_increment", 0.0
-            )
+            group["weight_decay"] = max(0,  group.get("weight_decay", 0.0) + group.get(
+                "weight_decay_increment", 0.0))
             self._finalize_group_step_metrics(group, metrics)
 
         return loss